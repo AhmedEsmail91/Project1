1ce6646b4bff6640cc518ce9b9661c7b97d2445b
AhmedEsmail91 <eng.ahmed.esmail.91@gmail.com>
2025-03-02 13:29:06 +0200

    bycrypt insatlled

--- modules/index.routes.js
+++ modules/index.routes.js
@@ -1,6 +1,6 @@
 import userRoutes from './user/user.routes.js';
 export const bootstrap = (app)=>{
-    app.use('/api/v1/',userRoutes);
+    app.use('/api/v1/users',userRoutes);
 
     app.use('*',(req, res, next) => {
         res.status(404).json({message:'Sorry, that route does not exist.'});
