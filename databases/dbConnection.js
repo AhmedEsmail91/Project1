cde57e6f5fc17823b2d37f639ef91d13e13db59e
AhmedEsmail91 <eng.ahmed.esmail.91@gmail.com>
2025-03-02 13:26:07 +0200

    adding mongo cluster connection

--- databases/dbConnection.js
+++ databases/dbConnection.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 export default function dbConnection(){
     // mongoose.set('strictQuery', true);// this is to avoid the warning of deprication.
-    mongoose.connect(process.env.DB_CONNECTION).then(()=>{
+    mongoose.connect("mongodb+srv://ahmedesmail9102:jP1gCvm36IpBRD19@cluster0.0omyp.mongodb.net/hosting").then(()=>{
         console.log(chalck.blue("Connected to MongoDB"));
     }).catch((err)=>{
         console.log('Error:',err);
